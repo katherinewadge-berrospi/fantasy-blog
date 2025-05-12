af4190853de7fe7ee3d247fcc97a16e014050b61
Katherine Wadge-Berrospi <katherinewadge@gmail.com>
2025-05-12 15:21:48 +0100

    Add metadata to Comment model.

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
     
@@ -35,4 +35,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"{self.body} | by {self.author}"
     
