480edd5f44430cac3933a63190592118bd6f6019
Katherine Wadge-Berrospi <katherinewadge@gmail.com>
2025-05-23 11:00:10 +0100

    Edit url pattern for about, refer to about/urls.py

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from about import views as about_views
 
 
 urlpatterns = [
-    path('about/', about_views.about_me, name='about'),
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
