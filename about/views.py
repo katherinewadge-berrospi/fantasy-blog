e47d65b19bbeba3b517fd1ce4f8282a588b4c511
Katherine Wadge-Berrospi <katherinewadge@gmail.com>
2025-06-03 16:29:13 +0100

    Edit display message.

--- about/views.py
+++ about/views.py
@@ -13,7 +13,7 @@ def about_me(request):
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I will try to respond within 2 working days.")
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
