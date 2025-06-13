233741d92b5d39ad25c15ee9e04cb74247e2a13a
Katherine Wadge-Berrospi <katherinewadge@gmail.com>
2025-06-13 13:00:33 +0100

    Add lost slash in URL.

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('like/<slug:slug>/', views.like_post, name='like_post'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
+    path('<slug:slug>/edit_comment/<int:comment_id>/',
          views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
+    path('<slug:slug>/delete_comment/<int:comment_id>/',
          views.comment_delete, name='comment_delete'),
 ]
